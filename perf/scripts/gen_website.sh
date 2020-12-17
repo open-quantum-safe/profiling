e18cdd71aecc4bf6418eea31d723f36372a2e104
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2020-12-17 22:45:00 +0100

    Rename speed to profiling (#26)

--- perf/scripts/gen_website.sh
+++ perf/scripts/gen_website.sh
@@ -2,7 +2,7 @@
 
 
 rm -rf tmp && mkdir tmp
-cd tmp && git clone https://github.com/open-quantum-safe/speed.git && cd ..
+cd tmp && git clone https://github.com/open-quantum-safe/profiling.git && cd ..
 rm -rf out && mkdir out
 python3 gen_website.py 2020-10-04 ${1} out
 cp tmp/speed/visualization/*.html out
