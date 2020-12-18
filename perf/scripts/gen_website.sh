6592bced6c07141e1ea0078fa5db961b5a4829bb
Michael <57787676+baentsch@users.noreply.github.com>
2020-12-18 12:13:19 +0100

    correct HTML copy path

--- perf/scripts/gen_website.sh
+++ perf/scripts/gen_website.sh
@@ -5,9 +5,9 @@ rm -rf tmp && mkdir tmp
 cd tmp && git clone https://github.com/open-quantum-safe/profiling.git && cd ..
 rm -rf out && mkdir out
 python3 gen_website.py 2020-10-04 ${1} out
-cp tmp/speed/visualization/*.html out
-cp tmp/speed/visualization/*.js out
-cp tmp/speed/visualization/*.css out
+cp tmp/profiling/visualization/*.html out
+cp tmp/profiling/visualization/*.js out
+cp tmp/profiling/visualization/*.css out
 cd out && tar czvf ../site.tgz * && cd ..
 cp site.tgz ${1}/site
 rm -rf tmp out
