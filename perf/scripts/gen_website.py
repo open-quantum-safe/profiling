1ba5ae14b4f0ba8347be28a0adba068526b16eba
Michael Baentsch <info@baentsch.ch>
2021-03-09 17:48:56 +0100

    adding x-build; ARM64 tested OK (#41)

--- perf/scripts/gen_website.py
+++ perf/scripts/gen_website.py
@@ -9,8 +9,8 @@ gen_files = [ "speed_kem", "speed_sig", "speed", "handshakes", "speed_kem-ref", 
 # will contain list of date/tgz-filename paths to be used for generating .list files:
 files = {}
 
-if len(sys.argv)!=4:
-   print("Usage: %s <startdate> <datafile-folder> <output-folder>" % (sys.argv[0]))
+if len(sys.argv)<4:
+   print("Usage: %s <startdate> <datafile-folder> <output-folder> [arch]" % (sys.argv[0]))
    exit(-1)
 
 # Parse start date (param1)
@@ -20,11 +20,15 @@ sd = datetime.datetime.strptime(sys.argv[1], '%Y-%m-%d')
 datadir = os.fsencode(sys.argv[2])
     
 outdir = sys.argv[3]
+try:
+   arch=sys.argv[4]
+except:
+   arch=""
     
 # collect all files after start date
 for file in os.listdir(datadir):
      filename = os.fsdecode(file)
-     if filename.endswith(".tgz"): 
+     if filename.endswith(arch+".tgz"):
          dt = datetime.datetime.strptime(filename[0:10], '%Y-%m-%d')
          if (dt > sd): 
             files[dt.date()]=os.path.join(sys.argv[2], filename)
--- perf/scripts/gen_website.sh
+++ perf/scripts/gen_website.sh
@@ -1,32 +1,34 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-   echo "Usage: $0 <datafolder>. Exiting."
+if [ $# -eq 0 ]; then
+   echo "Usage: $0 <datafolder> [arch]. Exiting."
    exit 1
 fi
 
 rm -rf tmp && mkdir tmp
 cd tmp && git clone https://github.com/open-quantum-safe/profiling.git && cd ..
 rm -rf out && mkdir out
-python3 gen_website.py 2020-10-04 ${1} out
+python3 gen_website.py 2020-10-04 ${1} out ${2}
 cp tmp/profiling/visualization/*.html out
 cp tmp/profiling/visualization/*.js out
 cp tmp/profiling/visualization/*.css out
-cd out && tar czvf ../site.tgz * && cd ..
-cp site.tgz ${1}/site
+cd out && tar czvf ../site${2}.tgz * && cd ..
+cp site${2}.tgz ${1}/site
 
 # perform deviation test and reporting
 
-# obtain the last deviation report
-cp $1/devs.txt .
-# create new deviations report
-python3 devcheck.py out devs.txt > newdevs.txt
-# discard cycle counting deviations assuming they're caused by swapping
-egrep -v "insts|cycles" newdevs.txt > report.txt
-# send email
-python3 notify.py report.txt
-# store todays deviation for comparison tomorrow
-cp newdevs.txt $1/devs.txt
+# obtain the last deviation report (only for x86_64)
+if [ $# -eq 1 ]; then
+  cp $1/devs.txt .
+  # create new deviations report
+  python3 devcheck.py out devs.txt > newdevs.txt
+  # discard cycle counting deviations assuming they're caused by swapping
+  egrep -v "insts|cycles" newdevs.txt > report.txt
+  # send email
+  python3 notify.py report.txt
+  # store todays deviation for comparison tomorrow
+  cp newdevs.txt $1/devs.txt
+fi
 
 # cleanup
 rm -rf tmp out report.txt *devs.txt
