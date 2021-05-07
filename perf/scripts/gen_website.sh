31ce2e17f4c3d26f15c04affa6db9de784458d91
Michael Baentsch <info@baentsch.ch>
2021-05-07 19:28:29 +0000

    more error-tolerant website content (#59)

--- perf/scripts/gen_website.sh
+++ perf/scripts/gen_website.sh
@@ -9,22 +9,13 @@ rm -rf tmp && mkdir tmp
 cd tmp && git clone https://github.com/open-quantum-safe/profiling.git && cd ..
 rm -rf out && mkdir out
 
-# To be removed when combiner works well:
-python3 gen_website.py 2021-03-25 ${1} out ${2}
-cp tmp/profiling/visualization/*.html out
-cp tmp/profiling/visualization/*.js out
-cp tmp/profiling/visualization/*.css out
-cd out && tar czvf ../site${2}.tgz * && cd ..
-cp site${2}.tgz ${1}/site
-
 # generate combined JSON and HTML:
-mkdir out2
-python3 combine.py 10 ${1} out2
+python3 combine.py 10 ${1} out
 if [ $? -eq 0 ]; then
-  cp tmp/profiling/visualization/*.html out2
-  cp tmp/profiling/visualization/*.js out2
-  cp tmp/profiling/visualization/*.css out2
-  cd out2 && tar czvf ../site-combined.tgz * && cd ..
+  cp tmp/profiling/visualization/*.html out
+  cp tmp/profiling/visualization/*.js out
+  cp tmp/profiling/visualization/*.css out
+  cd out && tar czvf ../site-combined.tgz * && cd ..
   cp site-combined.tgz ${1}/site
 fi
 # perform deviation test and reporting
@@ -32,7 +23,7 @@ fi
 # obtain the last deviation report (arch-dependent)
 cp $1/devs.txt$2 .
 # create new deviations report
-python3 devcheck.py out devs.txt$2 > newdevs.txt$2
+python3 devcheck.py $1 $2 devs.txt$2 > newdevs.txt$2 2>/dev/null
 # output architecture in report file:
 echo "Deviations for architecture: $2" > report.txt
 # discard cycle counting deviations assuming they're caused by swapping
@@ -43,5 +34,5 @@ cp newdevs.txt$2 $1/devs.txt$2
 python3 notify.py report.txt
 
 # cleanup
-rm -rf tmp out* report.txt *devs.txt*
+rm -rf tmp out report.txt *devs.txt*
 
